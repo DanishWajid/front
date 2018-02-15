6eae9d5594dfba2274e36fed318aef5c7fa64622
Danish Wajid <danish.wajid786@gmail.com>
2018-02-15 09:26:10 +0500

    List View Interface update

--- main_page.js
+++ main_page.js
@@ -4,22 +4,23 @@ const http = require('http').Server(app)
 app.get('/', function(req, res){
 
     if ('text' in req.query) {
+        document.getElementById("text_div").style.visibility="Visible"
+        document.getElementById("text").style.visibility="Visible"
         document.getElementById('text').innerHTML = req.query.text
-        document.getElementById("1").style.visibility="hidden";
-        document.getElementById("2").style.visibility="hidden";
-        document.getElementById("3").style.visibility="hidden";
-        document.getElementById("4").style.visibility="hidden";
-        document.getElementById("5").style.visibility="hidden";
-        document.getElementById("6").style.visibility="hidden";
-        document.getElementById("7").style.visibility="hidden";
-        document.getElementById("8").style.visibility="hidden";
 
+        for (var i = 1; i <=7; i++) {
+            document.getElementById("div"+i.toString()).style.visibility="hidden";
+            document.getElementById(i.toString()).style.visibility="hidden";
+            
+
+        }
     }
    if('list' in req.query){
-        var my_array = req.query.list.split(',')
-
+        var my_array = req.query.list.split('_')
+        document.getElementById("text_div").style.visibility="hidden"
+        document.getElementById("text").style.visibility="hidden"
         for (var i = 1; i <= my_array.length; i++) {
-            // document.getElementsByClassName(i.toString()).style.visibility="visibile";
+            document.getElementById("div"+i.toString()).style.visibility="Visible";
             document.getElementById(i.toString()).style.visibility="Visible";
             document.getElementById(i.toString()).innerHTML = my_array[i-1]
 
